a22496c95af31e525605a470c5d4781462eee38a
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-07-17 16:56:46 -0300

    Implementando Udate Funcionario

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,7 +1,23 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.views import View
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Funcionario
 
 
-def home(request):
-    return HttpResponse("Olá")
+class FuncionarioList(ListView):
+    model = Funcionario
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        queryset = Funcionario.objects.filter(empresa=empresa_logada)
+        return queryset
+
+
+class FuncionarioEdit(UpdateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
+
+    success_url = reverse_lazy('list_funcionarios')
+
+
