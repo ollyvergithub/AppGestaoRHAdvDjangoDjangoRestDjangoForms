c22c0c7b1c2100b6d02006f286a274988595e35d
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-07-17 15:35:53 -0300

    Implementando Create e Udate Empresa

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from apps.core import urls as core_urls
 from apps.funcionarios import urls as funcionarios_urls
+from apps.empresas import urls as empresa_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include(core_urls)),
     path('funcionarios/', include(funcionarios_urls)),
+    path('empresa/', include(empresa_urls)),
 ]
