999dcdb61ed7342f9bc9d7e682726a35d3358da8
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-07-18 11:35:14 -0300

    Finalizando CRUD Documentos

--- apps/documentos/admin.py
+++ apps/documentos/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Documento
 
-admin.site.register(Documento)
+
+class ListandoDocumentos(admin.ModelAdmin):
+    list_display = ('descricao', 'pertence', 'thumbnail')
+
+    def thumbnail(self, obj):
+        if obj.arquivo:
+            return format_html('<img src="{}" style="width: 50px"/>'.format(obj.arquivo.url))
+
+    thumbnail.short_description = "Foto do Cliente"
+
+
+admin.site.register(Documento, ListandoDocumentos)
--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -1,15 +1,21 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from apps.core import urls as core_urls
 from apps.funcionarios import urls as funcionarios_urls
 from apps.empresas import urls as empresa_urls
 from apps.departamentos import urls as departamento_urls
+from apps.documentos import urls as documento_urls
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include(core_urls)),
     path('funcionarios/', include(funcionarios_urls)),
     path('empresa/', include(empresa_urls)),
     path('departamentos/', include(departamento_urls)),
-]
+    path('documentos/', include(documento_urls)),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
