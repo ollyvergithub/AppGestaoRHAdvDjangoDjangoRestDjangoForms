44e5a46bc493483e290d329e18a224917416aac3
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-07-18 15:14:39 -0300

    MUITO BACANA REGISTRO DE HORA EXTRA - Filtrando somente os funcionários da empresa logada. Injetando o nosso proprio Formpara a class based views, tanto CreateView quanto UpdadeView

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import RegistroHoraExtra
+from .forms import RegistroHoraExtraForm
 
 
 class HoraExtraList(ListView):
@@ -15,19 +16,34 @@ class HoraExtraList(ListView):
 
 
 class HoraExtraNovo(CreateView):
-    model = RegistroHoraExtra
-    fields = "__all__"
-
+    # model = RegistroHoraExtra
+    # fields = ['motivo', 'horas']
     # def form_valid(self, form):
     #     hora_extra = form.save(commit=False)
     #     hora_extra.funcionario = self.request.user.funcionario
     #     hora_extra.save()
     #     return super(HoraExtraNovo, self).form_valid(form)
 
+    # *** Filtrando somente os funcionários da empresa logada ***
+    # *** Todas as generic based views, recebem o parametro form_class ***
+    # *** Injetando o nosso proprio Form
+    model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraNovo, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
+
 
 class HoraExtraEdit(UpdateView):
     model = RegistroHoraExtra
-    fields = "__all__"
+    form_class = RegistroHoraExtraForm
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraEdit, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
+
     success_url = reverse_lazy('list_hora_extra')
 
 
