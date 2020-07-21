e1dd92eea1413dc9d0b04cf1c18e3b5862d5da63
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-07-21 13:23:53 -0300

    Corrigindo caminho arquivos estaticos

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -92,13 +92,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    'static',
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
