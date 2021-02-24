1d36b0aeeaaa5f11d015e6db06044f0debe77c4f
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2021-02-24 10:45:25 -0300

    Implementando Token Autentication

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'bootstrapform',
     'apps.core',
     'apps.empresas',
